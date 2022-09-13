3fbbb5e5adc38bae6ac95cc4daef614938f23de6
Ihsan Khawaja <113303686+IhsanKhawaja@users.noreply.github.com>
2022-09-13 19:14:40 +0000

    added read me. wrote and created a functioning chatbot.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Elite-101-PreWork
+This is my Elite 101 Prework project chatbox
+You can tell it anything you want and ask it questions
+you can ask it questions with can you, do you, and are you
---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+exit = False #To manage when the user is done
+
+responses = ["How interesting.","That's cool!","Nice!","Neat!"] #To respond to the user making statements
+y_or_n = ["yes","no"] #To respond to asking questions
+
+print("Hey, i'm chatbot!\nWhen your ready to leave type exit.")
+
+while exit is False:
+  response = str.lower(input())
+  check2 = ''
+  check_question = []
+  index = 0
+  
+  if len(response) >= 6:
+    while index < 7:
+      check_question.append("".join(response[index]))
+      index += 1
+    check2 = "".join(check_question)
+  if response == "exit":
+    exit = True
+  elif check2 == "do you " or check2 == "can you" or check2 == "are you":
+    print(y_or_n[random.randint(0,1)])
+  else:
+    print(responses[random.randint(0,3)])
