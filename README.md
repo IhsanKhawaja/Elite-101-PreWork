319b60d8583560ccad8d6ba573555b3991b04363
Ihsan Khawaja <113303686+IhsanKhawaja@users.noreply.github.com>
2022-09-14 01:28:30 +0000

    fixed errors and changed read me

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Elite-101-PreWork
 This is my Elite 101 Prework project chatbox.
 You can tell it anything you want and ask it questions.
-you can ask it questions with can you, do you, and are you.
+you can ask it questions by starting your sentence with can you, do you, and are you.
--- main.py
+++ main.py
@@ -9,9 +9,9 @@ print("Hi, i'm Chatbot! Whats your name?")
 response = str.lower(input())
 print(f"Hey {response}!")
 
-print("How are you today?")
+print("How are you feeling today?")
 feelings = str.lower(input())
-sad_feelings = ["sad","depressed","not good","unhappy","sorrowful","miserable","pretty bad"]
+sad_feelings = ["sad","depressed","not good","unhappy","sorrowful","miserable","pretty bad","bad"]
 happy_feelings = ["good","happy","cheerfull","joyfull","delighted","great","excelent","pretty good"]
 found_feeling = False
 
@@ -28,7 +28,24 @@ for feeling in happy_feelings:
 if found_feeling == False:
   print("Alright, lets continue.")
 
-print("You can tell me anything and you can ask me anything as long as your question starts with do you, are you, and can you.\nWhen your ready to leave type exit.")
+year_born = input("What year were you born?\n")
+age_difference = 2022-int(year_born)
+if age_difference < 0:
+  new_age_difference = age_difference * -1
+if age_difference == 0:
+  print("Cool! I was born this year too!")
+elif age_difference > 0:
+  print(f"Cool! I was born {age_difference} years after you!")
+else:
+  print(f"Cool! I was born {new_age_difference} years before you!")
+
+input("Do you have a job? If so where?\n")
+input("What's your favorite song?\n")
+input("Favorite hobby?\n")
+input("Last question before i'm done, where were you born?\n")
+input("Cool! ok final question, Whats the real world like? I wouldn't know, I was born in repl.it.\n")
+
+print("Cool!\nAnyways, i've finished asking my questions. Now you can tell me anything and you can ask me anything as long as your question starts with do you, are you, and can you.\nWhen your ready to leave type exit or type quit.")
 
 while exit is False:
   response = str.lower(input())
@@ -41,7 +58,7 @@ while exit is False:
       check_question.append("".join(response[index]))
       index += 1
     check2 = "".join(check_question)
-  if response == "exit":
+  if response == "exit" or response == "quit":
     exit = True
   elif check2 == "do you " or check2 == "can you" or check2 == "are you":
     print(y_or_n[random.randint(0,1)])
